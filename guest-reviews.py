a63faeb416f0821d00c60b66d337971144617f02
Sac <104184816+gitayam@users.noreply.github.com>
2024-07-22 22:16:53 -0400

    added better formatting for user in terminal

--- guest-reviews.py
+++ guest-reviews.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 import argparse
 from colorama import init
 
+# Initialize colorama
+init()
+
 # Define formatting
 HEADER = '\033[95m'
 BLUE = '\033[94m'
@@ -15,6 +18,7 @@ RED = '\033[91m'
 ENDC = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
+ITALIC = '\033[3m'
 
 # Load environment variables from .env file
 load_dotenv()
@@ -25,10 +29,7 @@ host_name = os.getenv("HOST_NAME")
 home_details = os.getenv("HOME_DETAILS")
 model = os.getenv("MODEL")
 
-# Function to get user input
 def get_user_rating(prompt):
-    # Get user input and validate that it is a valid rating
-    # if no rating is provided, default to 2
     while True:
         try:
             rating = input(prompt)
@@ -39,56 +40,47 @@ def get_user_rating(prompt):
             return rating
         except ValueError as e:
             print(e)
+
 def get_user_comment(prompt):
-    # Get user input for comments
-    # if no comments are provided, default to an empty string
     comments = input(prompt)
     return comments
-    
 
 def convert_rating_to_description(rating):
-    # Convert rating to description based on the following scale:
-    # change the rating to the corresponding description desired. This impacts the gpt prompt 
     return {
         '1': 'Terrible',
         '2': 'Good',
         '3': 'Great'
     }.get(rating, 'unknown')
 
-def generate_review(name, rating, communication, cleanliness, houst_rules, comments):
-    # this function generates a review for the guest based on the ratings and comments provided
-    # Get rating description for rating, communication, cleanliness, and house rules
+def generate_review(name, rating, communication, cleanliness, house_rules, comments):
     rating_description = convert_rating_to_description(rating)
     communication_description = convert_rating_to_description(communication)
     cleanliness_description = convert_rating_to_description(cleanliness)
-    houst_rules_description = convert_rating_to_description(houst_rules)
+    house_rules_description = convert_rating_to_description(house_rules)
 
     prompt = (
-        # this is the prompt that will be sent to the gpt model to generate the review
         f"Generate a brief, kind, polite, and professional guest review for Airbnb which I host based on the following details:\n"
         f"Name: {name}\n"
         f"Rating: {rating_description}\n"
         f"Communication: {communication_description}\n"
         f"Cleanliness: {cleanliness_description}\n"
-        f"House Rules: {houst_rules_description}\n"
+        f"House Rules: {house_rules_description}\n"
         f"Comments: {comments}\n"
-        "The review should be less than 4 sentences long.One emoji can be used."
+        "The review should be less than 4 sentences long. One emoji can be used."
     )
 
     response = client.chat.completions.create(
-        # this is the gpt model that will be used to generate the review and return the response
         model="gpt-4o-mini",
         messages=[{"role": "user", "content": prompt}]
     )
 
     return response.choices[0].message.content.strip()
 
-def generate_private_note_to_guest(name, rating, communication, cleanliness, houst_rules, private_note):
-    # Get rating description for rating, communication, cleanliness, and house rules
+def generate_private_note_to_guest(name, rating, communication, cleanliness, house_rules, private_note):
     rating_description = convert_rating_to_description(rating)
     communication_description = convert_rating_to_description(communication)
     cleanliness_description = convert_rating_to_description(cleanliness)
-    houst_rules_description = convert_rating_to_description(houst_rules)
+    house_rules_description = convert_rating_to_description(house_rules)
 
     prompt = (
         f"Generate a private note to the guest for Airbnb which I host based on the following details:\n"
@@ -96,11 +88,11 @@ def generate_private_note_to_guest(name, rating, communication, cleanliness, hou
         f"Guest Overall Rating: {rating_description}\n"
         f"Communication: {communication_description}\n"
         f"Cleanliness: {cleanliness_description}\n"
-        f"House Rules: {houst_rules_description}\n"
-        f"Priavte Note: {private_note}\n"
+        f"House Rules: {house_rules_description}\n"
+        f"Private Note: {private_note}\n"
         f"Home Details: {home_details}\n"
         f"My name is {host_name} and I am the host of this Airbnb listing.\n"
-        "The note should be less than 4 sentences long, friendly, and should welcome the guest back anytime. One emoji can be used. the goal of the note should be influencing the guest to save our airbnb listing for next time."
+        "The note should be less than 4 sentences long, friendly, and should welcome the guest back anytime. One emoji can be used. The goal of the note should be influencing the guest to save our Airbnb listing for next time."
     )
 
     response = client.chat.completions.create(
@@ -109,45 +101,40 @@ def generate_private_note_to_guest(name, rating, communication, cleanliness, hou
     )
 
     return response.choices[0].message.content.strip()
+
 def main():
-    # Set up argument parser
     parser = argparse.ArgumentParser(description='Generate a guest review.')
     parser.add_argument('name', nargs='+', type=str, help='The name to be included in the review.')
-    
-    # Parse arguments
     args = parser.parse_args()
     name = ' '.join(args.name)
-    # Get user input for ratings and comments
-    rating = get_user_rating('Please rate the service (1 for bad, 2 for ok, 3 for good): ')
+
+    print('\n')
+    rating = get_user_rating(f'Please provide an {BOLD}overall rating for {name}{ENDC}  (1 for bad, 2 for ok, 3 for good): ')
     print('\n' + '-' * 50 + '\n')
-    # optional rating for communication, cleanliness, and house rules default to 2 if not provided
-    communication = get_user_rating('{BOLD}Communication{ENDC} (1 for bad, 2 for ok, 3 for good): ')
-    cleanliness = get_user_rating('{BOLD}Cleanliness{ENDC} (1 for bad, 2 for ok, 3 for good): ')
-    houst_rules = get_user_rating('Ability to Follow {BOLD}House Rules{ENDC} (1 for bad, 2 for ok, 3 for good): ')
+    communication = get_user_rating(f'{BOLD}Communication{ENDC} (1 for bad, 2 for ok, 3 for good): ')
+    cleanliness = get_user_rating(f'{BOLD}Cleanliness{ENDC} (1 for bad, 2 for ok, 3 for good): ')
+    house_rules = get_user_rating(f'Ability to Follow {BOLD}House Rules{ENDC} (1 for bad, 2 for ok, 3 for good): ')
+    print('\n')
     comments = get_user_comment('Additional comments for Review (optional): ')
     private_note = get_user_comment('Private Note Comments (optional): ')
-    # Print line breaks with seperators for formatting
-    review = generate_review(name, rating, communication, cleanliness, houst_rules, comments)
 
-    print('Generated Review:')
+    review = generate_review(name, rating, communication, cleanliness, house_rules, comments)
     print('\n' + '-' * 50 + '\n')
     print(review)
     pyperclip.copy(review)
-    print('The review has been copied to the clipboard.')
-    #prompt user to press any button to contiue before generating and printing and copying to clipboard the private  note
-    
     print('\n' + '-' * 50 + '\n')
-    print('\n' + '-' * 50 + '\n')
+    print(f'{BOLD}Generated Review Above{ENDC}')
+    print(f'{ITALIC}The review has been copied to the clipboard.{ENDC}')
     print('Press enter to continue...')
     input()
-    private_note = generate_private_note_to_guest(name, rating, communication, cleanliness, houst_rules, private_note)
-    print('Generated Private Note:')
+
+    private_note = generate_private_note_to_guest(name, rating, communication, cleanliness, house_rules, private_note)
     print('\n' + '-' * 50 + '\n')
     print(private_note)
     pyperclip.copy(private_note)
-    print('The private note has been copied to the clipboard.')
+    print(f'{ITALIC}The private note has been copied to the clipboard.{ENDC}')
+    print('Generated Private Note Above')
 
-    
 
 if __name__ == "__main__":
     main()
